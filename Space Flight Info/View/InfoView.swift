3281e456cbcccbed0423d1f054c0499f4f26c7db
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-08-17 16:35:57 +0600

    Updating InfoView.swift with a complete view

--- Space Flight Info/View/InfoView.swift
+++ Space Flight Info/View/InfoView.swift
@@ -1,1 +1,39 @@
 import Foundation
+
+struct InfoView : View {
+
+  // object of class SpaceAPI declared in APIFormation.swift
+  @EnvironmentObject var data = SpaceAPI
+  // variable to open URLs in the system browser
+  @Environment(\.openURL) var openURL
+
+  var body : some View {
+    List {
+      ForEach(data.news){ news in
+        // looping through the articles with specific information
+        InfoArticle(
+          title: news.title,
+          imageURL: news.imageUrl,
+          siteName: news.newsSite,
+          summary: news.summary
+        )
+        // clicks redirect to specific URLs
+        .onTapGesture{
+        openURL(URL(string: news.url)!)
+                     }
+      }
+    }
+    // this marks a view as refreshable
+    .refreshable{
+      data.getData()
+    }
+  }
+
+}
+// preview
+    struct InfoView_Previews: PreviewProvider {
+        static var previews: some View {
+            InfoView()
+                .environmentObject(SpaceAPI())
+        }
+    }
